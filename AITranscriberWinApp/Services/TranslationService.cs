9c0069d157bad081f588f6a90ca33f3145d0e99e
Pournaeim <pournaeim@gmail.com>
2025-10-15 18:54:49 -0400

    Ensure translation requests use TLS 1.2

--- AITranscriberWinApp/Services/TranslationService.cs
+++ AITranscriberWinApp/Services/TranslationService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -20,6 +21,7 @@ namespace AITranscriberWinApp.Services
         public TranslationService(Uri endpoint)
         {
             _endpoint = endpoint ?? throw new ArgumentNullException(nameof(endpoint));
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
             _httpClient = new HttpClient();
         }
 
