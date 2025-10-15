e4d8942f9b9acaf79762ab8483fa7e1edeb4b58f
DESKTOP-JIAU3D3\Sina <pournaeim@gmail.com>
2025-10-15 18:35:03 -0400

    updated

--- AITranscriberWinApp/Services/OpenAiTranscriptionService.cs
+++ AITranscriberWinApp/Services/OpenAiTranscriptionService.cs
@@ -36,7 +36,20 @@ namespace AITranscriberWinApp.Services
 
                 using (var form = new MultipartFormDataContent())
                 {
-                    var fileBytes = await File.ReadAllBytesAsync(audioPath, cancellationToken).ConfigureAwait(false);
+                    byte[] fileBytes;
+                    using (var fs = new FileStream(audioPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
+                    {
+                        fileBytes = new byte[fs.Length];
+                        int bytesRead = 0;
+                        while (bytesRead < fileBytes.Length)
+                        {
+                            int read = await fs.ReadAsync(fileBytes, bytesRead, fileBytes.Length - bytesRead, cancellationToken).ConfigureAwait(false);
+                            if (read == 0)
+                                break;
+                            bytesRead += read;
+                        }
+                    }
+
                     var fileContent = new ByteArrayContent(fileBytes);
                     fileContent.Headers.ContentType = new MediaTypeHeaderValue("audio/wav");
 
