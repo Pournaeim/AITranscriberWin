81c395df17341b3595c2fe3f9a05f1d43284376b
Pournaeim <pournaeim@gmail.com>
2025-10-19 11:39:26 -0400

    Disambiguate translation error helper names

--- AITranscriberWinApp/MainForm.Designer.cs
+++ AITranscriberWinApp/MainForm.Designer.cs
@@ -31,6 +31,8 @@ namespace AITranscriberWinApp
             this.lblApiKey = new System.Windows.Forms.Label();
             this.txtApiKey = new System.Windows.Forms.TextBox();
             this.btnSaveKey = new System.Windows.Forms.Button();
+            this.lblTranslationEndpoint = new System.Windows.Forms.Label();
+            this.txtTranslationEndpoint = new System.Windows.Forms.TextBox();
             this.btnToggleRecording = new System.Windows.Forms.Button();
             this.btnSelectAudio = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
@@ -61,23 +63,41 @@ namespace AITranscriberWinApp
             this.txtApiKey.UseSystemPasswordChar = true;
             // 
             // btnSaveKey
-            // 
+            //
             this.btnSaveKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSaveKey.Location = new System.Drawing.Point(555, 10);
+            this.btnSaveKey.Location = new System.Drawing.Point(555, 38);
             this.btnSaveKey.Name = "btnSaveKey";
             this.btnSaveKey.Size = new System.Drawing.Size(123, 27);
-            this.btnSaveKey.TabIndex = 2;
-            this.btnSaveKey.Text = "Save Key";
+            this.btnSaveKey.TabIndex = 4;
+            this.btnSaveKey.Text = "Save Settings";
             this.btnSaveKey.UseVisualStyleBackColor = true;
             this.btnSaveKey.Click += new System.EventHandler(this.btnSaveKey_Click);
+            //
+            // lblTranslationEndpoint
+            //
+            this.lblTranslationEndpoint.AutoSize = true;
+            this.lblTranslationEndpoint.Location = new System.Drawing.Point(13, 43);
+            this.lblTranslationEndpoint.Name = "lblTranslationEndpoint";
+            this.lblTranslationEndpoint.Size = new System.Drawing.Size(163, 17);
+            this.lblTranslationEndpoint.TabIndex = 2;
+            this.lblTranslationEndpoint.Text = "Translation Service URL:";
+            //
+            // txtTranslationEndpoint
+            //
+            this.txtTranslationEndpoint.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTranslationEndpoint.Location = new System.Drawing.Point(182, 40);
+            this.txtTranslationEndpoint.Name = "txtTranslationEndpoint";
+            this.txtTranslationEndpoint.Size = new System.Drawing.Size(367, 22);
+            this.txtTranslationEndpoint.TabIndex = 3;
             // 
             // btnToggleRecording
             // 
             this.btnToggleRecording.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnToggleRecording.Location = new System.Drawing.Point(555, 43);
+            this.btnToggleRecording.Location = new System.Drawing.Point(555, 75);
             this.btnToggleRecording.Name = "btnToggleRecording";
             this.btnToggleRecording.Size = new System.Drawing.Size(123, 32);
-            this.btnToggleRecording.TabIndex = 3;
+            this.btnToggleRecording.TabIndex = 6;
             this.btnToggleRecording.Text = "Start Recording";
             this.btnToggleRecording.UseVisualStyleBackColor = true;
             this.btnToggleRecording.Click += new System.EventHandler(this.btnToggleRecording_Click);
@@ -85,10 +105,10 @@ namespace AITranscriberWinApp
             // btnSelectAudio
             //
             this.btnSelectAudio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectAudio.Location = new System.Drawing.Point(426, 43);
+            this.btnSelectAudio.Location = new System.Drawing.Point(426, 75);
             this.btnSelectAudio.Name = "btnSelectAudio";
             this.btnSelectAudio.Size = new System.Drawing.Size(123, 32);
-            this.btnSelectAudio.TabIndex = 4;
+            this.btnSelectAudio.TabIndex = 5;
             this.btnSelectAudio.Text = "Transcribe File...";
             this.btnSelectAudio.UseVisualStyleBackColor = true;
             this.btnSelectAudio.Click += new System.EventHandler(this.btnSelectAudio_Click);
@@ -97,10 +117,10 @@ namespace AITranscriberWinApp
             //
             this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblStatus.Location = new System.Drawing.Point(13, 48);
+            this.lblStatus.Location = new System.Drawing.Point(13, 80);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(407, 23);
-            this.lblStatus.TabIndex = 5;
+            this.lblStatus.TabIndex = 7;
             this.lblStatus.Text = "Status: Idle";
             //
             // txtTranscript
@@ -114,7 +134,7 @@ namespace AITranscriberWinApp
             this.txtTranscript.ReadOnly = true;
             this.txtTranscript.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtTranscript.Size = new System.Drawing.Size(326, 328);
-            this.txtTranscript.TabIndex = 6;
+            this.txtTranscript.TabIndex = 8;
             //
             // txtTranslation
             //
@@ -126,24 +146,24 @@ namespace AITranscriberWinApp
             this.txtTranslation.ReadOnly = true;
             this.txtTranslation.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtTranslation.Size = new System.Drawing.Size(330, 328);
-            this.txtTranslation.TabIndex = 7;
+            this.txtTranslation.TabIndex = 9;
             //
             // lblTranscript
             //
             this.lblTranscript.AutoSize = true;
-            this.lblTranscript.Location = new System.Drawing.Point(13, 90);
+            this.lblTranscript.Location = new System.Drawing.Point(13, 110);
             this.lblTranscript.Name = "lblTranscript";
             this.lblTranscript.Size = new System.Drawing.Size(144, 17);
-            this.lblTranscript.TabIndex = 8;
+            this.lblTranscript.TabIndex = 10;
             this.lblTranscript.Text = "English Transcript:";
             //
             // lblTranslation
             //
             this.lblTranslation.AutoSize = true;
-            this.lblTranslation.Location = new System.Drawing.Point(345, 90);
+            this.lblTranslation.Location = new System.Drawing.Point(345, 110);
             this.lblTranslation.Name = "lblTranslation";
             this.lblTranslation.Size = new System.Drawing.Size(132, 17);
-            this.lblTranslation.TabIndex = 9;
+            this.lblTranslation.TabIndex = 11;
             this.lblTranslation.Text = "Persian Translation:";
             //
             // btnOpenOutputFolder
@@ -152,7 +172,7 @@ namespace AITranscriberWinApp
             this.btnOpenOutputFolder.Location = new System.Drawing.Point(555, 444);
             this.btnOpenOutputFolder.Name = "btnOpenOutputFolder";
             this.btnOpenOutputFolder.Size = new System.Drawing.Size(123, 29);
-            this.btnOpenOutputFolder.TabIndex = 10;
+            this.btnOpenOutputFolder.TabIndex = 12;
             this.btnOpenOutputFolder.Text = "Open Folder";
             this.btnOpenOutputFolder.UseVisualStyleBackColor = true;
             this.btnOpenOutputFolder.Click += new System.EventHandler(this.btnOpenOutputFolder_Click);
@@ -162,6 +182,8 @@ namespace AITranscriberWinApp
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(690, 485);
+            this.Controls.Add(this.txtTranslationEndpoint);
+            this.Controls.Add(this.lblTranslationEndpoint);
             this.Controls.Add(this.btnSelectAudio);
             this.Controls.Add(this.btnOpenOutputFolder);
             this.Controls.Add(this.lblTranslation);
@@ -195,5 +217,7 @@ namespace AITranscriberWinApp
         private System.Windows.Forms.Label lblTranscript;
         private System.Windows.Forms.Label lblTranslation;
         private System.Windows.Forms.Button btnOpenOutputFolder;
+        private System.Windows.Forms.Label lblTranslationEndpoint;
+        private System.Windows.Forms.TextBox txtTranslationEndpoint;
     }
 }
